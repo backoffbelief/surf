d2ce1abc183ffd0b55b798a8f7e64033bd93fc87
backoffbelief <backoffbelief@gmail.com>
2015-04-16 10:45:19 +0800

    array/system

--- surf_util/src/main/java/com/kael/util/RandUtil.java
+++ surf_util/src/main/java/com/kael/util/RandUtil.java
@@ -34,4 +34,42 @@ public class RandUtil {
 	public static int rand(int max){
 		return rand(0,max) ;
 	}
+	
+	public static int randInArray(int[] array){
+		int sum = 0;
+		for (int i : array) {
+			sum += i;
+		}
+		
+		if(sum <= 0){
+			throw new RuntimeException(String.format("[sum(array)=%d] <= 0", sum));
+		}
+		
+	    final int r = rand(sum);
+	    
+	    sum = 0;
+	    for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+			if(r < sum ){
+				return i;
+			}
+		}
+	    throw new RuntimeException("cannot be here!");
+	}
+	
+	
+	public static boolean nextBoolean(){
+		return getR().nextBoolean();
+	}
+	
+	public static float nextFloat(){
+		return getR().nextFloat();
+	}
+	
+	/*
+	 * @param f% 
+	 */
+	public static boolean randWithPercentage(float f){
+	    return getR().nextFloat() < f / 100 ;
+	}
 }
