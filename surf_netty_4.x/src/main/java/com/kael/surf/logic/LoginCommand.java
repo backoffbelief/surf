ece3aff7730582dffb712b9e803e76aed5022a9f
backoffbelief <backoffbelief@gmail.com>
2015-05-05 18:19:10 +0800

    codec

---
+++ surf_netty_4.x/src/main/java/com/kael/surf/logic/LoginCommand.java
@@ -0,0 +1,15 @@
+package com.kael.surf.logic;
+
+import com.kael.surf.net.AppPlayer;
+import com.kael.surf.net.Cmd;
+import com.kael.surf.net.IMessage;
+import com.kael.surf.net.QueueCommad;
+@Cmd(code=1,protoName="")
+public class LoginCommand extends QueueCommad<AppPlayer> {
+
+	@Override
+	public void exec(AppPlayer player, IMessage msg) {
+		
+	}
+
+}
