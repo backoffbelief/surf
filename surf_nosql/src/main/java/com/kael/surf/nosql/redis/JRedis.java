20877b2d46fedfec5144acc089292065beb12ef7
backoffbelief <backoffbelief@gmail.com>
2015-05-11 17:07:35 +0800

    redis的常用命令学习

--- surf_nosql/src/main/java/com/kael/surf/nosql/redis/JRedis.java
+++ surf_nosql/src/main/java/com/kael/surf/nosql/redis/JRedis.java
@@ -2,153 +2,183 @@ package com.kael.surf.nosql.redis;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import redis.clients.jedis.Tuple;
 
 public interface JRedis {
 
 	//connection
 	void auth(String pwd);
 	
-	//string
-	void append(String key,String value);
-	void append(byte[] key,String value);
-	
 	void bgrewriteaof();
 	
-	int dbsize();
+	Long dbsize();
 	
-	long decr(String key);
+	//事务
+	void muti(String key);
 	
-	long decr(byte[] key);
+	void watch(String key);
 	
-	long decrby(String key, int decrment);
+	void exec();
+	
+	void unwatch();
+		
+	
+	//string
+	Long append(String key,String value);
+	
+	Long append(byte[] key,byte[] value);
+	
+	Long decr(String key);
+	
+	Long decr(byte[] key);
+	
+	Long decrby(String key, int decrment);
 
-	long decrby(byte[] key, int decrment);
+	Long decrby(byte[] key, int decrment);
 	
-	void del(String key);
+	Long del(String... keys);
 	
-	void del(byte[] key);
+	Long del(byte[]... keys);
 	
 	boolean exists(String key);
 
 	boolean exists(byte[] key);
 	
-	void expire(String key, int seconds);
+	Long expire(String key, int seconds);
 	
-	void expireat(String key,long millseconds);
+	Long expireAt(String key,long millseconds);
 	
 	/**
 	 * remove the expiration from a key
 	 * @param key
 	 */
-	void persist(String key);
+	Long persist(String key);
 	
-	void pexpire(String key,int seconds);
+	Long pexpire(String key,long seconds);
 	
-	void pexpireat(String key, long millseconds);
+	Long pexpireAt(String key, long millseconds);
 	
-	void psetex(String key,long millseconds,String value);
+	String psetex(String key,long millseconds,String value);
 	
 	long pttl(String key);
 
-	void expire(byte[] key, int seconds);
+	long ttl(String key);
 	
-	void expireat(byte[] key,long millseconds);
+	Long expire(byte[] key, int seconds);
+	
+	Long expireat(byte[] key,long millseconds);
 	
 	/**
 	 * remove the expiration from a key
 	 * @param key
 	 */
-	void persist(byte[] key);
+	Long persist(byte[] key);
 	
-	void pexpire(byte[] key,int seconds);
+	Long pexpire(byte[] key,long seconds);
 	
-	void pexpireat(byte[] key, long millseconds);
+	Long pexpireAt(byte[] key, long millseconds);
 	
-	void psetex(byte[] key,long millseconds,byte[] value);
+	String psetex(byte[] key,long millseconds,byte[] value);
 	
 	long pttl(byte[] key);
 	
+	long ttl(byte[] key);
 	
 	/**
 	 * remove all keys from all db
 	 */
-	void flushall();
+	String flushAll();
 
 	/**
 	 * remove all keys from current db
 	 */
-	void flushdb();
+	String flushDB();
 	
 	String get(String key);
 	
 	byte[] get(byte[] key);
 	
-	String getbit(String key, int offset);
+    String set(String key,String value);
+	
+    String set(byte[] key,byte[] value);
+	
+	Boolean getbit(String key, long offset);
 	
 	String getrange(String key, int start,int end);
 	
-	String getset(String key,String value);
+	String getSet(String key,String value);
 
-	byte[] getset(byte[] key,byte[] value);
+	byte[] getSet(byte[] key,byte[] value);
 	
-	String randomStrKey();
+    String setex(String key,int seconds,String value);
+    
+    Long setnx(String key,String value);
+	
+    String setex(byte[] key,int seconds,byte[] value);
+    
+    Long setnx(byte[] key,byte[] value);
+    
+    
+	String randomKey();
 
-	byte[] randomBytesKey();
+//	byte[] randomBytesKey();
 	
 	//hashs
 	
-	void hdel(String key,String... fields);
+	Long hdel(String key,String... fields);
 	
 	boolean hexist(String key, String field);
 	
 	String hget(String key,String field);
 	
-	Map<String, String> hgetall(String key);
+	Map<String, String> hgetAll(String key);
 	
-	Long hincrby(String key,String field,int increment);
+	Long hincrBy(String key,String field,int increment);
 	
-	float hincrbyfloat(String key,String field, float increment);
+	Double hincrByFloat(String key,String field, double increment);
 	
-	List<String> hkeys(String key);
+	Set<String> hkeys(String key);
 	
 	Long hlen(String key);
 	
-	Map<String, String> hmget(String key, String... fields);
+	List<String> hmget(String key, String... fields);
 	
-	long hmset(String key,Map<String, String> kvstr);
+	String hmset(String key,Map<String, String> kvstr);
 	
 	long hset(String key,String field,String value);
 	
-	boolean hsetinx(String key,String field,String value);
+	Long hsetnx(String key,String field,String value);
 	
-	long hstrlen(String key,String field);
+//	long hstrlen(String key,String field);
 	
 	List<String> hvals(String key);
 	
 	
-    void hdel(byte[] key,byte[]... fields);
+    Long hdel(byte[] key,byte[]... fields);
 	
 	boolean hexist(byte[] key, byte[] field);
 	
 	byte[] hget(byte[] key,byte[] field);
 	
-	Map<byte[], byte[]> hgetall(byte[] key);
+	Map<byte[], byte[]> hgetAll(byte[] key);
 	
 	Long hincrby(byte[] key,byte[] field,int increment);
 	
-	float hincrbyfloat(byte[] key,byte[] field, float increment);
+	double hincrByFloat(byte[] key,byte[] field, double increment);
 	
-	List<byte[]> hkeys(byte[] key);
+	Set<byte[]> hkeys(byte[] key);
 	
 	Long hlen(byte[] key);
 	
-	Map<byte[], byte[]> hmget(byte[] key, byte[]... fields);
+	List<byte[]> hmget(byte[] key, byte[]... fields);
 	
-	long hmset(byte[] key,Map<byte[], byte[]>  kvBytes);
+	String hmset(byte[] key,Map<byte[], byte[]>  kvBytes);
 	
 	long hset(byte[] key,byte[] field,byte[] value);
 	
-	boolean hsetinx(byte[] key,byte[] field,byte[] value);
+	Long hsetnx(byte[] key,byte[] field,byte[] value);
 	
 	//long hstrlen(byte[] key,byte[] field);
 	
@@ -206,17 +236,31 @@ public interface JRedis {
 	
 	long incr(byte[] key);
 	
-	long incrby(String key,int increment);
+	long incrBy(String key,int increment);
 
-	long incrby(byte[] key,int increment);
+	long incrBy(byte[] key,int increment);
 	
-	float incrbyfloat(String key,float increment);
+	Double incrByFloat(String key,double increment);
 	
-	float incrbyfloat(byte[] key,float increment);
+	Double incrByFloat(byte[] key,double increment);
 
-	List<String> keys(String pattern);
+	Set<String> keys(String pattern);
 
-	List<byte[]> keys(byte[] pattern);
+	Set<byte[]> keys(byte[] pattern);
+	
+	List<String> mget(String... keys);
+	
+	String mset(String... keysvalues);
+	
+	Long msetnx(String... msetnx);
+
+	
+	List<byte[]> mget(byte[]... keys);
+	
+	String mset(byte[]... keysvalues);
+	
+	Long msetnx(byte[]... msetnx);
+	
 	/**
 	 * get unix time stamp of last successful save to disk
 	 * @return
@@ -225,84 +269,196 @@ public interface JRedis {
 	
 	
 	//list
-	String lindex(String key,int index);
+	String lindex(String key,long index);
 
-	int llen(String key);
+	Long llen(String key);
 	
-	long lpop(String key);
+	String lpop(String key);
 	
-	void lpush(String key,String... values);
+	Long lpush(String key,String... values);
 	
 	/**
 	 * prepend a value to a list  only if this list exists
 	 * @param key
 	 * @param value
 	 */
-	void lpushhx(String key,String value);
+	Long lpushx(String key,String... string);
 	
-	List<String> lrange(String key, int start,int stop);
+	List<String> lrange(String key, long start,long end);
 	
-	void lrem(String key,int count,String value);
+	Long lrem(String key,int count,String value);
 	
-	long lset(String key,int index, String value);
+	String lset(String key,int index, String value);
 	
-	List<String> ltrim(String key,int start,int stop);
+	String ltrim(String key,long start,long end);
 	
 	String rpop(String key);
-	//String rpoplpush(String key);
-	String rpushStrs(Map<String, String> kvStrs);
 	
-	void rpushx(String key,String value);
+	String rpoplpush(String srcKey, String dstKey);
+	
+	Long rpush(String key, String... strings);
+	
+	Long rpushx(String key,String value);
 	
 	
-	byte[] lindex(byte[] key,int index);
+	byte[] lindex(byte[] key,long index);
 	
-	int llen(byte[] key);
+	Long llen(byte[] key);
 	
-	long lpop(byte[] key);
+	byte[] lpop(byte[] key);
 	
-	void lpush(byte[] key,byte[]... values);
+	Long lpush(byte[] key,byte[]... values);
 	
 	/**
 	 * prepend a value to a list  only if this list exists
 	 * @param key
 	 * @param value
 	 */
-	boolean lpushhx(byte[] key,byte[] value);
+	Long lpushx(byte[] key,byte[]... values);
 	
-	List<byte[]> lrange(byte[] key, int start,int stop);
+	List<byte[]> lrange(byte[] key, long start,long stop);
 	
 	long lrem(byte[] key,int count,byte[] value);
 	
-	long lset(byte[] key,int index, byte[] value);
+	String lset(byte[] key,int index, byte[] value);
 	
-	List<byte[]> ltrim(byte[] key,int start,int stop);
+	String ltrim(byte[] key,long start,long stop);
 	
 	byte[] rpop(byte[] key);
-	//String rpoplpush(String key);
-	byte[] rpushBytes(Map<byte[], byte[]> kvBytes);
-	
-	void rpushx(byte[] key,byte[] value);
-	//
-    
-//	void migrate(String host,int port,String key)
-	
-	Map<String, String> mget(String... keys);
-	
-	long mset(Map<String, String> kvstrs);
-	
-	boolean msetnx(Map<String, String> kvstrs);
 
+	Long rpush(byte[] key, byte[]... bytes);
 	
-	Map<byte[], byte[]> mget(byte[]... keys);
+	Long rpushx(byte[] key,byte[] value);
+
+	byte[] rpoplpush(byte[] srcKey, byte[] dstKey);
+
+	//set
+	long sadd(String key,String... values);
 	
-	long msetBytes(Map<byte[], byte[]> kvBytes);
+	long scard(String key);
 	
-	boolean msetnxBytes(Map<byte[], byte[]> kvBytes);
+	boolean sismember(String key,String member);
+	
+	Set<String> smembers(String key);
+	
+	String spop(String key);
+
+	Set<String> spop(String key,int count);
+	
+	String srandmember(String key);
+	
+	List<String> srandmember(String key,int count);
+	
+	long srem(String key,String... values);
 	
 	
+	long sadd(byte[] key,byte[]... values);
 	
+	long scard(byte[] key);
 	
-	//事务
-	void muti(String key);
+	boolean sismember(byte[] key,byte[] member);
+	
+	Set<byte[]> smembers(byte[] key);
+	
+	byte[] spop(byte[] key);
+	
+	Set<byte[]> spop(byte[] key,int count);
+	
+	byte[] srandmember(byte[] key);
+	
+	List<byte[]> srandmember(byte[] key,int count);
+	
+	long srem(byte[] key,byte[]... values);
+	
+	//sortset
+	long zadd(String key, double score, String member);
+	
+	long zadd(String key, Map<String, Double> scoreMembers);
+	
+	long zcard(String key);
+	
+	long zcount(String key,double minScore,double maxScore);
+	
+	double zincrby(String key,double increment,String member);
+	
+	Set<String> zrange(String key,long start,long stop);
+
+	Set<Tuple> zrangeWithScores(String key, long start, long end);
+	
+	Set<String> zrangeByLex(String key,String min,String max);
+
+	Set<String> zrangeByLex(String key,String min,String max, int offset, int count);
+	
+	Set<String> zrevrangeByLex(String key,String max ,String min);
+
+	Set<String> zrevrangeByLex(String key, String max, String min, int offset, int count);
+	
+	Set<String> zrangeByScore(String key, double min, double max);
+	
+	Set<Tuple> zrangeByScoreWithScores(String key,double min ,double max);
+	
+	Long zrank(String key,String member);
+	
+	Long zrem(String key, String member);
+	
+	Long zremrangeByLex(String key,String min,String max);
+	
+	Long zremrangeByRank(String key,int start,int stop);
+	
+	Long zremrangebyscore(String key,double min,double max);
+	
+	Set<String> zrevrange(String key,int start,int stop);
+	
+	Set<Tuple> zrevrangeWithScores(String key,int min,int max);
+	
+	Long zrevrank(String key,String member);
+	
+	double zscore(String key,String member);
+	
+
+	long zadd(byte[] key, double score, byte[] member);
+
+	long zadd(byte[] key, Map<byte[], Double> scoreMembers);
+
+	long zcard(byte[] key);
+
+	long zcount(byte[] key, double minScore, double maxScore);
+
+	double zincrby(byte[] key, double increment, byte[] member);
+
+	Set<byte[]> zrange(byte[] key, long start, long end);
+
+	Set<Tuple> zrangeWithScores(byte[] key, long start, long end);
+
+	Set<byte[]> zrangeByLex(byte[] key, byte[] min, byte[] max);
+
+	Set<byte[]> zrangeByLex(byte[] key, byte[] min, byte[] max, int offset,
+			int count);
+
+	Set<byte[]> zrevrangeByLex(byte[] key, byte[] max, byte[] min);
+
+	Set<byte[]> zrevrangeByLex(byte[] key, byte[] max, byte[] min, int offset,
+			int count);
+
+	Set<byte[]> zrangeByScore(byte[] key, double min, double max);
+
+	Set<Tuple> zrangeByScoreWithScores(byte[] key, double min, double max);
+
+	Long zrank(byte[] key, byte[] member);
+
+	Long zrem(byte[] key, byte[] member);
+
+	Long zremrangeByLex(byte[] key, byte[] min, byte[] max);
+
+	Long zremrangeByRank(byte[] key, int start, int end);
+
+	Long zremrangebyscore(byte[] key, double start, double end);
+
+	Set<byte[]> zrevrange(byte[] key, int start, int end);
+
+	Set<Tuple> zrevrangeWithScores(byte[] key, int start, int end);
+
+	Long zrevrank(byte[] key, byte[] member);
+
+	double zscore(byte[] key, byte[] member);
 }
