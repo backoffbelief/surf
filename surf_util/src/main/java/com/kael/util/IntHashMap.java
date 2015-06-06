1059977cebee64a6ee3b7abe889b0cadcbd7086f
backoffbelief <backoffbelief@gmail.com>
2015-06-06 16:08:38 +0800

    inthashmap

--- surf_util/src/main/java/com/kael/util/IntHashMap.java
+++ surf_util/src/main/java/com/kael/util/IntHashMap.java
@@ -1,328 +1,366 @@
 package com.kael.util;
-public class IntHashMap {
 
-    /**
-     * The hash table data.
-     */
-    private transient Entry table[];
+import java.util.AbstractCollection;
+import java.util.AbstractSet;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
 
-    /**
-     * The total number of entries in the hash table.
-     */
-    private transient int count;
+/**
+ * 定制特殊的hashMap
+ * 
+ * 使用int原始类型做为key 以提高大量使用map时的性能
+ * 
+ * 不支持keySet方法 因为key为原始int类型
+ * 
+ * @param <T>
+ */
+@SuppressWarnings("unchecked")
+public class IntHashMap<T> {
 
-    /**
-     * The table is rehashed when its size exceeds this threshold.  (The
-     * value of this field is (int)(capacity * loadFactor).)
-     *
-     * @serial
-     */
-    private int threshold;
+	private static final int DEFAULT_INITIAL_CAPACITY = 32;
 
-    /**
-     * The load factor for the hashtable.
-     *
-     * @serial
-     */
-    private final float loadFactor;
+	private static final float DEFAULT_LOAD_FACTOR = 0.75f;
 
-    /**
-     * <p>Innerclass that acts as a datastructure to create a new entry in the
-     * table.</p>
-     */
-    private static class Entry {
-        final int hash;
-        final int key; // TODO not read; seems to be always same as hash
-        Object value;
-        Entry next;
+	private static final int MAXIMUM_CAPACITY = 1 << 30;
 
-        /**
-         * <p>Create a new entry with the given values.</p>
-         *
-         * @param hash The code used to hash the object with
-         * @param key The key used to enter this in the table
-         * @param value The value for this key
-         * @param next A reference to the next entry in the table
-         */
-        protected Entry(int hash, int key, Object value, Entry next) {
-            this.hash = hash;
-            this.key = key;
-            this.value = value;
-            this.next = next;
+	private transient Entry<T>[] table;
+
+	private transient int size;
+
+	private int threshold;
+
+	private final float loadFactor;
+	
+	private transient volatile int modCount;
+
+	public static class Entry<T> {
+		final int hash;
+		final int key;// hash also key, not used key
+		T value;
+		Entry<T> next;
+
+		Entry(int hash, int key, T value, Entry<T> next) {
+			this.hash = hash;
+			this.key = key;
+			this.value = value;
+			this.next = next;
+		}
+
+		public int getKey() {
+			return key;
+		}
+
+		public T getValue() {
+			return value;
+		}
+
+		public void setValue(T value) {
+			this.value = value;
+		}
+		
+	}
+
+	public IntHashMap() {
+		this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR);
+	}
+
+	public IntHashMap(int initialCapacity) {
+		this(initialCapacity, DEFAULT_LOAD_FACTOR);
+	}
+
+	public IntHashMap(int initialCapacity, float loadFactor) {
+		super();
+		if (initialCapacity < 0) {
+			throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+		}
+		if (loadFactor <= 0) {
+			throw new IllegalArgumentException("Illegal Load: " + loadFactor);
+		}
+		if (initialCapacity == 0) {
+			initialCapacity = 1;
+		}
+		if (initialCapacity > MAXIMUM_CAPACITY) {
+			initialCapacity = MAXIMUM_CAPACITY;
+		}
+
+		this.loadFactor = loadFactor;
+		table = new Entry[initialCapacity];
+		threshold = (int) (initialCapacity * loadFactor);
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public boolean contains(Object value) {
+		if (value == null) {
+			return containsNullValue();
+		}
+
+		Entry<T>[] tab = table;
+		for (int i = tab.length; i-- > 0;) {
+			for (Entry<T> e = tab[i]; e != null; e = e.next) {
+				if (e.value.equals(value)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean containsNullValue() {
+		Entry<T>[] tab = table;
+		for (int i = 0; i < tab.length; i++)
+			for (Entry<T> e = tab[i]; e != null; e = e.next)
+				if (e.value == null)
+					return true;
+		return false;
+	}
+
+	final Entry<T> getEntry(int key) {
+		int hash = key;
+		for (Entry<T> e = table[indexFor(hash, table.length)]; e != null; e = e.next) {
+			if (e.hash == hash)
+				return e;
+		}
+		return null;
+	}
+
+	public boolean containsKey(int key) {
+		return getEntry(key) != null;
+	}
+
+	static int indexFor(int h, int length) {
+		return h & (length - 1);
+	}
+
+	public T get(int key) {
+		Entry<T> tab[] = table;
+		int hash = key;
+		int index = indexFor(hash, table.length);
+		for (Entry<T> e = tab[index]; e != null; e = e.next) {
+			if (e.hash == hash) {
+				return e.value;
+			}
+		}
+		return null;
+	}
+
+	void resize(int newCapacity) {
+		Entry<T>[] oldTable = table;
+		int oldCapacity = oldTable.length;
+		if (oldCapacity == MAXIMUM_CAPACITY) {
+			threshold = Integer.MAX_VALUE;
+			return;
+		}
+
+		Entry<T>[] newTable = new Entry[newCapacity];
+		transfer(newTable);
+		table = newTable;
+		threshold = (int) (newCapacity * loadFactor);
+	}
+
+	void transfer(Entry<T>[] newTable) {
+		Entry<T>[] src = table;
+		int newCapacity = newTable.length;
+		for (int j = 0; j < src.length; j++) {
+			Entry<T> e = src[j];
+			if (e != null) {
+				src[j] = null;
+				do {
+					Entry<T> next = e.next;
+					int i = indexFor(e.hash, newCapacity);
+					e.next = newTable[i];
+					newTable[i] = e;
+					e = next;
+				} while (e != null);
+			}
+		}
+	}
+
+	public T put(int key, T value) {
+		Entry<T> tab[] = table;
+		int hash = key;
+		int index = indexFor(hash, table.length);
+		for (Entry<T> e = tab[index]; e != null; e = e.next) {
+			if (e.hash == hash) {
+				T old = e.value;
+				e.value = value;
+				return old;
+			}
+		}
+
+		modCount++;
+		addEntry(hash, key, value, index);
+		return null;
+	}
+
+	void addEntry(int hash, int key, T value, int bucketIndex) {
+		Entry<T> e = table[bucketIndex];
+		table[bucketIndex] = new Entry<T>(hash, key, value, e);
+		if (++size > threshold) {
+			resize(2 * table.length);
+		}
+	}
+
+	public boolean containsValue(Object value) {
+		return contains(value);
+	}
+
+	public T remove(int key) {
+		Entry<T> tab[] = table;
+		int hash = key;
+		int index = indexFor(hash, table.length);
+		for (Entry<T> e = tab[index], prev = null; e != null; prev = e, e = e.next) {
+			if (e.hash == hash) {
+				modCount ++;
+				if (prev != null) {
+					prev.next = e.next;
+				} else {
+					tab[index] = e.next;
+				}
+				size--;
+				T oldValue = e.value;
+				e.value = null;
+				return oldValue;
+			}
+		}
+		return null;
+	}
+
+	public void clear() {
+		modCount ++;
+		Entry<T> tab[] = table;
+		for (int index = tab.length; --index >= 0;) {
+			tab[index] = null;
+		}
+		size = 0;
+	}
+	
+	transient volatile Collection<T> values = null;
+	
+    public Collection<T> values() {
+        Collection<T> vs = values;
+        return (vs != null ? vs : (values = new Values()));
+    }
+    
+    private final class Values extends AbstractCollection<T> {
+        public Iterator<T> iterator() {
+            return new ValueIterator();
+        }
+        public int size() {
+            return size;
+        }
+        public boolean contains(Object o) {
+            return containsValue(o);
+        }
+        public void clear() {
+            IntHashMap.this.clear();
         }
     }
-
-    /**
-     * <p>Constructs a new, empty hashtable with a default capacity and load
-     * factor, which is <code>20</code> and <code>0.75</code> respectively.</p>
-     */
-    public IntHashMap() {
-        this(20, 0.75f);
+    
+    private final class ValueIterator extends IntHashIterator<T>{
+    	@Override
+    	public T next() {
+    		return nextEntry().value;
+    	}
     }
+    
+    private abstract class IntHashIterator<V> implements Iterator<V> {
+    	Entry<T> next;
+        int expectedModCount;
+        int index;
+        Entry<T> current;
 
-    /**
-     * <p>Constructs a new, empty hashtable with the specified initial capacity
-     * and default load factor, which is <code>0.75</code>.</p>
-     *
-     * @param  initialCapacity the initial capacity of the hashtable.
-     * @throws IllegalArgumentException if the initial capacity is less
-     *   than zero.
-     */
-    public IntHashMap(int initialCapacity) {
-        this(initialCapacity, 0.75f);
-    }
-
-    /**
-     * <p>Constructs a new, empty hashtable with the specified initial
-     * capacity and the specified load factor.</p>
-     *
-     * @param initialCapacity the initial capacity of the hashtable.
-     * @param loadFactor the load factor of the hashtable.
-     * @throws IllegalArgumentException  if the initial capacity is less
-     *             than zero, or if the load factor is nonpositive.
-     */
-    public IntHashMap(int initialCapacity, float loadFactor) {
-        super();
-        if (initialCapacity < 0) {
-            throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
-        }
-        if (loadFactor <= 0) {
-            throw new IllegalArgumentException("Illegal Load: " + loadFactor);
-        }
-        if (initialCapacity == 0) {
-            initialCapacity = 1;
-        }
-
-        this.loadFactor = loadFactor;
-        table = new Entry[initialCapacity];
-        threshold = (int) (initialCapacity * loadFactor);
-    }
-
-    /**
-     * <p>Returns the number of keys in this hashtable.</p>
-     *
-     * @return  the number of keys in this hashtable.
-     */
-    public int size() {
-        return count;
-    }
-
-    /**
-     * <p>Tests if this hashtable maps no keys to values.</p>
-     *
-     * @return  <code>true</code> if this hashtable maps no keys to values;
-     *          <code>false</code> otherwise.
-     */
-    public boolean isEmpty() {
-        return count == 0;
-    }
-
-    /**
-     * <p>Tests if some key maps into the specified value in this hashtable.
-     * This operation is more expensive than the <code>containsKey</code>
-     * method.</p>
-     *
-     * <p>Note that this method is identical in functionality to containsValue,
-     * (which is part of the Map interface in the collections framework).</p>
-     *
-     * @param      value   a value to search for.
-     * @return     <code>true</code> if and only if some key maps to the
-     *             <code>value</code> argument in this hashtable as
-     *             determined by the <tt>equals</tt> method;
-     *             <code>false</code> otherwise.
-     * @throws  NullPointerException  if the value is <code>null</code>.
-     * @see        #containsKey(int)
-     * @see        #containsValue(Object)
-     * @see        java.util.Map
-     */
-    public boolean contains(Object value) {
-        if (value == null) {
-            throw new NullPointerException();
-        }
-
-        Entry tab[] = table;
-        for (int i = tab.length; i-- > 0;) {
-            for (Entry e = tab[i]; e != null; e = e.next) {
-                if (e.value.equals(value)) {
-                    return true;
-                }
+        IntHashIterator() {
+            expectedModCount = modCount;
+            if (size > 0) {
+                Entry<T>[] t = table;
+                while (index < t.length && (next = t[index++]) == null);
             }
         }
-        return false;
-    }
 
-    /**
-     * <p>Returns <code>true</code> if this HashMap maps one or more keys
-     * to this value.</p>
-     *
-     * <p>Note that this method is identical in functionality to contains
-     * (which predates the Map interface).</p>
-     *
-     * @param value value whose presence in this HashMap is to be tested.
-     * @return boolean <code>true</code> if the value is contained
-     * @see    java.util.Map
-     * @since JDK1.2
-     */
-    public boolean containsValue(Object value) {
-        return contains(value);
-    }
-
-    /**
-     * <p>Tests if the specified object is a key in this hashtable.</p>
-     *
-     * @param  key  possible key.
-     * @return <code>true</code> if and only if the specified object is a
-     *    key in this hashtable, as determined by the <tt>equals</tt>
-     *    method; <code>false</code> otherwise.
-     * @see #contains(Object)
-     */
-    public boolean containsKey(int key) {
-        Entry tab[] = table;
-        int hash = key;
-        int index = (hash & 0x7FFFFFFF) % tab.length;
-        for (Entry e = tab[index]; e != null; e = e.next) {
-            if (e.hash == hash) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * <p>Returns the value to which the specified key is mapped in this map.</p>
-     *
-     * @param   key   a key in the hashtable.
-     * @return  the value to which the key is mapped in this hashtable;
-     *          <code>null</code> if the key is not mapped to any value in
-     *          this hashtable.
-     * @see     #put(int, Object)
-     */
-    public Object get(int key) {
-        Entry tab[] = table;
-        int hash = key;
-        int index = (hash & 0x7FFFFFFF) % tab.length;
-        for (Entry e = tab[index]; e != null; e = e.next) {
-            if (e.hash == hash) {
-                return e.value;
-            }
-        }
-        return null;
-    }
-
-    /**
-     * <p>Increases the capacity of and internally reorganizes this
-     * hashtable, in order to accommodate and access its entries more
-     * efficiently.</p>
-     *
-     * <p>This method is called automatically when the number of keys
-     * in the hashtable exceeds this hashtable's capacity and load
-     * factor.</p>
-     */
-    protected void rehash() {
-        int oldCapacity = table.length;
-        Entry oldMap[] = table;
-
-        int newCapacity = oldCapacity * 2 + 1;
-        Entry newMap[] = new Entry[newCapacity];
-
-        threshold = (int) (newCapacity * loadFactor);
-        table = newMap;
-
-        for (int i = oldCapacity; i-- > 0;) {
-            for (Entry old = oldMap[i]; old != null;) {
-                Entry e = old;
-                old = old.next;
-
-                int index = (e.hash & 0x7FFFFFFF) % newCapacity;
-                e.next = newMap[index];
-                newMap[index] = e;
-            }
-        }
-    }
-
-    /**
-     * <p>Maps the specified <code>key</code> to the specified
-     * <code>value</code> in this hashtable. The key cannot be
-     * <code>null</code>. </p>
-     *
-     * <p>The value can be retrieved by calling the <code>get</code> method
-     * with a key that is equal to the original key.</p>
-     *
-     * @param key     the hashtable key.
-     * @param value   the value.
-     * @return the previous value of the specified key in this hashtable,
-     *         or <code>null</code> if it did not have one.
-     * @throws  NullPointerException  if the key is <code>null</code>.
-     * @see     #get(int)
-     */
-    public Object put(int key, Object value) {
-        // Makes sure the key is not already in the hashtable.
-        Entry tab[] = table;
-        int hash = key;
-        int index = (hash & 0x7FFFFFFF) % tab.length;
-        for (Entry e = tab[index]; e != null; e = e.next) {
-            if (e.hash == hash) {
-                Object old = e.value;
-                e.value = value;
-                return old;
-            }
+        public final boolean hasNext() {
+            return next != null;
         }
 
-        if (count >= threshold) {
-            // Rehash the table if the threshold is exceeded
-            rehash();
+        final Entry<T> nextEntry() {
+        	if (modCount != expectedModCount)
+        		throw new ConcurrentModificationException();
+        	Entry<T> e = next;
+        	if (e == null)
+        		throw new NoSuchElementException();
 
-            tab = table;
-            index = (hash & 0x7FFFFFFF) % tab.length;
+        	if ((next = e.next) == null) {
+        		Entry<T>[] t = table;
+        		while (index < t.length && (next = t[index++]) == null)
+        			;
+        	}
+        	current = e;
+        	return e;
         }
 
-        // Creates the new entry.
-        Entry e = new Entry(hash, key, value, tab[index]);
-        tab[index] = e;
-        count++;
-        return null;
+        public void remove() {
+            if (current == null)
+                throw new IllegalStateException();
+            if (modCount != expectedModCount)
+                throw new ConcurrentModificationException();
+            int k = current.key;
+            current = null;
+            IntHashMap.this.remove(k);
+            expectedModCount = modCount;
+        }
+
+    }
+    
+    private transient Set<Entry<T>> entrySet = null;
+    
+    public Set<Entry<T>> entrySet() {
+	    Set<Entry<T>> es = entrySet;
+	    return es != null ? es : (entrySet = new EntrySet());
     }
 
-    /**
-     * <p>Removes the key (and its corresponding value) from this
-     * hashtable.</p>
-     *
-     * <p>This method does nothing if the key is not present in the
-     * hashtable.</p>
-     *
-     * @param   key   the key that needs to be removed.
-     * @return  the value to which the key had been mapped in this hashtable,
-     *          or <code>null</code> if the key did not have a mapping.
-     */
-    public Object remove(int key) {
-        Entry tab[] = table;
-        int hash = key;
-        int index = (hash & 0x7FFFFFFF) % tab.length;
-        for (Entry e = tab[index], prev = null; e != null; prev = e, e = e.next) {
-            if (e.hash == hash) {
-                if (prev != null) {
-                    prev.next = e.next;
-                } else {
-                    tab[index] = e.next;
-                }
-                count--;
-                Object oldValue = e.value;
-                e.value = null;
-                return oldValue;
-            }
+    private final class EntrySet extends AbstractSet<Entry<T>> {
+        public Iterator<Entry<T>> iterator() {
+            return new EntryIterator();
         }
-        return null;
+        public boolean contains(Object o) {
+            if (!(o instanceof Map.Entry))
+                return false;
+            
+            Entry<T> e = (Entry<T>) o;
+            Entry<T> candidate = getEntry(e.getKey());
+            return candidate != null && candidate.equals(e);
+        }
+        
+        public boolean remove(Object o) {
+            if (!(o instanceof Entry))
+                return false;
+            
+            return IntHashMap.this.remove(((Entry<T>) o ).key) != null;
+        }
+        public int size() {
+            return size;
+        }
+        
+        public void clear() {
+            IntHashMap.this.clear();
+        }
     }
-
-    /**
-     * <p>Clears this hashtable so that it contains no keys.</p>
-     */
-    public synchronized void clear() {
-        Entry tab[] = table;
-        for (int index = tab.length; --index >= 0;) {
-            tab[index] = null;
-        }
-        count = 0;
+    
+    private final class EntryIterator extends IntHashIterator<Entry<T>>{
+		@Override
+		public Entry<T> next() {
+			return nextEntry();
+		}
     }
     
 }
